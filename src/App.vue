1325264d5f3b308d80f7f30b82fba853a7dc8b79
Shahid-gds <shahid.gdsdev@gmail.com>
2023-03-07 21:16:23 -0800

    Muftar Mobile Resp

---
+++ src/App.vue
@@ -0,0 +1,271 @@
+<template>
+  <div id="app">
+    <the-header v-if="!mobileView"></the-header>
+    <div class="content" v-if="mobileView" :class="{'open':showNav}">
+    <div id="navigation-icon" v-if="mobileView" @click="showNav=!showNav">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+        />
+      </svg>
+       <MobileNavigation v-if="mobileView"/>
+    </div>
+  </div>
+    </div>
+    <div>
+       <router-view></router-view>
+  </div>
+  <div>
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+import MobileNavigation from "./components/layout/MobileNavigation.vue";
+import TheFooter from "./components/layout/TheFooter.vue";
+export default {
+  components: {
+    TheHeader,
+    MobileNavigation,
+    TheFooter,
+  },
+  data() {
+    return {
+      mobileView: true,
+      showNav: true,
+    };
+  },
+  methods: {
+    handleView(){
+      this.mobileView = window.innerWidth <= 990;
+    }
+  },
+  created(){
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+html,
+body{
+  overflow: hidden;
+}
+#navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+  margin-left: -5rem;
+  position: absolute;
+  margin-top: 2.5rem;
+}
+.content svg {
+  width: 50px;
+  height: 20px;
+  position: absolute;
+  margin-top: -2.5rem;
+  margin-left: -2rem;
+}
+
+.content {
+  position: absolute;
+  top: 10px;
+  padding: 20px;
+  background-color: lightblue;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
+  height: 40rem;
+  width: 26rem;
+  transition: 1s transform cubic-bezier(0,.12,.14,1);
+}
+
+.open {
+  transform: translate(410px);
+}
+@media (max-width: 990px) {
+  html,
+body{
+  overflow: hidden;
+}
+  #navigation-icon {
+    padding: 10px 10px 20px;
+    margin-right: 10px;
+    cursor: pointer;
+    margin-left: -5rem;
+    position: absolute;
+    margin-top: 2.5rem;
+  }
+  .content svg {
+    width: 50px;
+    height: 20px;
+    position: absolute;
+    margin-top: -2.5rem;
+    margin-left: -2rem;
+  }
+  
+  .content {
+    position: absolute;
+    top: 10px;
+    padding: 20px;
+    background-color: lightblue;
+    border-radius: 30px;
+    box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
+    height: 40rem;
+    width: 26rem;
+    margin-left: 37rem;
+    transition: 1s transform cubic-bezier(0,.12,.14,1);
+  }
+  
+  .open {
+    transform: translate(410px);
+  }
+}
+@media (max-width: 900px) {
+  html,
+body{
+  overflow: hidden;
+}
+  #navigation-icon {
+    padding: 10px 10px 20px;
+    margin-right: 10px;
+    cursor: pointer;
+    margin-left: -5rem;
+    position: absolute;
+    margin-top: 2.5rem;
+  }
+  .content svg {
+    width: 50px;
+    height: 20px;
+    position: absolute;
+    margin-top: -2.5rem;
+    margin-left: -2rem;
+  }
+  
+  .content {
+    position: absolute;
+    top: 10px;
+    padding: 20px;
+    background-color: lightblue;
+    border-radius: 30px;
+    box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
+    height: 40rem;
+    width: 26rem;
+    margin-left: 37rem;
+    transition: 1s transform cubic-bezier(0,.12,.14,1);
+  }
+  
+  .open {
+    transform: translate(410px);
+  }
+}
+@media (max-width: 912px) {
+  html,
+body{
+  overflow: hidden;
+}
+  
+  .content {
+      margin-left: 32rem;
+  
+  }
+  
+}
+@media (max-width: 768px) {
+  html,
+body{
+  overflow: hidden;
+}
+  
+  .content {
+      margin-left: 23rem;
+  
+  }
+  
+}
+@media (max-width: 700px) {
+  html,
+body{
+  overflow: hidden;
+}
+  #navigation-icon {
+    padding: 10px 10px 20px;
+    margin-right: 10px;
+    cursor: pointer;
+    margin-left: -5rem;
+    position: absolute;
+    margin-top: 2.5rem;
+  }
+  .content svg {
+    width: 50px;
+    height: 20px;
+    position: absolute;
+    margin-top: -2.5rem;
+    margin-left: -2rem;
+  }
+  
+  .content {
+    position: absolute;
+    top: 10px;
+    padding: 20px;
+    background-color: lightblue;
+    border-radius: 30px;
+    box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
+    height: 40rem;
+    width: 26rem;
+    margin-left: 37rem;
+    transition: 1s transform cubic-bezier(0,.12,.14,1);
+  }
+  
+  .open {
+    transform: translate(410px);
+  }
+}
+@media (max-width: 540px) {
+  html,
+body{
+  overflow: hidden;
+}
+  
+  .content {
+      margin-left: 8rem;
+  
+  }
+  
+}
+@media (max-width: 412px) {
+  html,
+body{
+  overflow: hidden;
+}
+  
+  .content {
+    margin-left: 0rem;
+  }
+
+}
+@media (max-width: 414px) {
+  html,
+body{
+  overflow: hidden;
+}
+  
+  .content {
+    margin-left: 0rem;
+  }
+
+}
+</style>
---
+++ src/pages/TheHome.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="mob-logo">
+    <router-link to="/">
+      <img src="../assets/images/logo.svg" alt="" />
+    </router-link>
+  </div>
+  <section>
+    <div class="right_box">
+      <h1>Powerful & Paperless, Yet Simple</h1>
+      <p>
+        Speed up your business processes and make sure <br />
+        your business operations are running smoothly.
+      </p>
+      <div class="all_checkboxes">
+        <img src="../assets/images/process.svg" alt="">
+    </div>
+    <h3>Stay informed when new updates are released.</h3>
+    <div class="short_form">
+        <input type="email" placeholder="Enter Your Email">
+        <button>Join Our Waitlist</button>
+    </div>
+    </div>
+    <div class="left_box">
+        <img src="../assets/images/Divices.svg" alt="">
+    </div>
+  </section>
+</template>
+
+<style scoped>
+html,
+body{
+  overflow: hidden;
+}
+.mob-logo{
+  display: none;
+}
+section {
+  display: flex;
+}
+.right_box {
+  margin: 5rem 12rem;
+}
+.right_box h1 {
+  font-size: 40px;
+  font-weight: 400;
+  color: #242021;
+}
+.right_box p{
+  color: #666668;
+  margin-top: 2.5rem;
+  font-size: 22px;
+  font-weight: 400;
+}
+.left_box {
+  margin: -7.5rem -6rem;
+}
+.all_checkboxes {
+  display: flex;
+  align-items: center;
+  margin: 9rem 0rem;
+}
+.right_box h3{
+    color: #666668;
+    margin-top: 8rem;
+    font-weight: 400;
+}
+.short_form {
+    margin-top: 2rem;
+}
+.short_form input[type='email']{
+    padding: 1rem 2rem;
+    font-size: 18px;
+    border-radius: 10px;
+    width: 25rem;
+}
+button {
+    margin-left: 2rem;
+    padding: 1rem 2rem;
+    background-color: #3c58a8;
+    border: none;
+    outline: none;
+    color: white;
+    font-size: 18px;
+    border-radius: 10px;
+    cursor: pointer;
+}
+@media (max-width: 1366px) {
+  html,
+body{
+  overflow: hidden;
+}
+ .mob-logo {
+  display: none;
+ }
+  section {
+    display: flex;
+  }
+    .right_box{
+      margin: 4rem 4rem;
+    }
+    .left_box{
+      margin: -14rem -8rem;
+    }
+    .left_box img{
+        width: 65rem;
+        height: 60rem;
+    }
+    .all_checkboxes {
+      display: flex;
+      align-items: center;
+      margin: 4rem 0rem;
+    }
+    .right_box h3 {
+      margin-top: 1rem;
+    }
+    .short_form {
+      margin-top: 1rem;
+  }
+}
+@media (max-width: 1280px) {
+  html,
+body{
+  overflow: hidden;
+}
+ .mob-logo {
+  display: none;
+ }
+  section {
+    display: flex;
+  }
+    .right_box{
+      margin: 4rem 4rem;
+    }
+    .left_box{
+      margin: -14rem -8rem;
+    }
+    .left_box img{
+        width: 55rem;
+        height: 60rem;
+    }
+    .all_checkboxes {
+      display: flex;
+      align-items: center;
+      margin: 4rem 0rem;
+    }
+    .right_box h3 {
+      margin-top: 1rem;
+    }
+    .short_form {
+      margin-top: 1rem;
+  }
+}
+@media(max-width:990px){
+  .all_checkboxes img {
+    width: 550px;
+  }
+  button {
+    margin-left: 6rem;
+    margin-top: 1rem
+}
+.left_box img {
+  width: 45rem;
+}
+}
+@media(max-width:700px)
+
+  {
+    html,
+    body{
+      overflow: hidden;
+    }
+    .mob-logo{
+      display: block;
+    }
+    .mob-logo{
+      margin-top: 2.5rem;
+      margin-left: 1rem;
+    }
+    .mob-logo img{
+     width: 150px;
+    }
+    .right_box {
+      margin: 2rem 2rem;
+      width: 80%;
+  }
+    .right_box h1 {
+      font-size: 25px;
+      font-weight: 400;
+      color: #242021;
+  }
+  .right_box p{
+       font-size: 13px;
+ 
+}
+  .all_checkboxes {
+    display: flex;
+    align-items: center;
+    margin: 4rem 0rem;
+    flex-direction: column;
+}
+.all_checkboxes img{
+  width: 100%;
+}
+.left_box{
+  margin: -5rem -27rem;
+}
+.left_box img {
+  width: 30rem;
+}
+.right_box h3 {
+  margin-top: 18rem;
+}
+.short_form input[type='email'] {
+  padding: 0.5rem 1rem;
+  font-size: 15px;
+  border-radius: 10px;
+  width: 19rem;
+}
+button {
+  margin-left: 0rem;
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: #3c58a8;
+  border: none;
+  outline: none;
+  color: white;
+  font-size: 14px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+}
+</style>
